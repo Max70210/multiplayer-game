2b9307c1c2603823f0988e70216e536410dab585
Dhaval Bhatt <dhavalbhatt@Dhavals-MacBook-Air.local>
2026-02-14 17:42:31 +0530

    multiplayer join system

--- server.js
+++ server.js
@@ -8,6 +8,35 @@ const io = new Server(server);
 
 app.use(express.static("public"));
 
+let rooms = {};
+
+io.on("connection", (socket) => {
+
+    console.log("User connected:", socket.id);
+
+    socket.on("joinRoom", ({ roomCode, name }) => {
+
+        socket.join(roomCode);
+
+        if (!rooms[roomCode]) {
+            rooms[roomCode] = {
+                players: {},
+                secretNumber: null,
+                gameActive: false
+            };
+        }
+
+        rooms[roomCode].players[socket.id] = {
+            name,
+            score: 0,
+            attempts: 0
+        };
+
+        io.to(roomCode).emit("updatePlayers", rooms[roomCode].players);
+    });
+
+});
+    
 server.listen(process.env.PORT || 3000, () => {
     console.log("Server running...");
 });
